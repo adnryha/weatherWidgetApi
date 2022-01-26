948a89b2f16c0b34a944d2ec5fa0901931cf9dac
Andrii Tyrmorezov <tyrmorezovandre@gmail.com>
2022-01-26 12:58:47 +0300

    fixed http for https in fetch

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let cityName = prompt('Введіть місто (Львів, Київ...)');
 console.log(cityName);
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=16b6754ad8952f29340c4a77785bfb10&lang=uk`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=16b6754ad8952f29340c4a77785bfb10&lang=uk`)
   .then(function (resp) { return resp.json() })
   .then(function (data) {
     document.querySelector('.package-name').innerHTML = data.name;
@@ -10,7 +10,7 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric
     document.querySelector('.humidity').innerHTML = '<img src="https://img.icons8.com/ios-filled/50/26e07f/humidity.png"/>' + ' ' + data.main.humidity + '%';
     document.querySelector('.wind').innerHTML = '<img src="https://img.icons8.com/ios-filled/50/26e07f/azimuth--v5.png"/>' + ' ' + data.wind.speed + 'м/с';
     document.querySelector('.wind_deg').innerHTML = '<img src="https://img.icons8.com/ios/50/26e07f/windsock.png"/>' + ' ' + data.wind.deg + '&deg;';
-    document.querySelector('.features li').innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0]['icon']}.png">`;
+    document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0]['icon']}.png">`;
     document.querySelector('.disclaimer').innerHTML = 'Опис:' + 'Сьогодні в ' + ' ' + cityName + ' ' + 'очікуються' + ' ' + data.weather[0]['description'] + '.';
 
   })
